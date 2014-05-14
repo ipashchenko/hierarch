24cf788d875bdcff7a137e1c875f6e3264203be6
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-05-14 16:58:08 +0400

    Data loaded in kinematic.py

--- kinematic.py
+++ kinematic.py
@@ -15,6 +15,79 @@ class Dlognorm_shifted(object):
     def __call__(self, **kwargs):
         return self.shift + np.random.lognormal(**kwargs)
 
+
+class LnPost(object):
+    """
+    Class that represents log of posterior density for kinematic model.
+    """
+    def __init__(self, beta_ij, lnpr=None):
+        self._lnlike = LnLike(beta_ij)
+        self._lnpr = lnpr
+
+    def __call__(self, p):
+        return self._lnlike(p) + self._lnpr(p)
+
+
+class LnLike(object):
+    """
+    Class that represents ln of likelihood for kinematic model.
+    """
+
+    @staticmethod
+    def ln_of_normpdf(x, loc=0., logscale=0.):
+        """
+        Returns ln of pdf for normal distribution at points x.
+
+        :param x:
+            Value (or array of values) for which to calculate the result.
+
+        :param loc (optional):
+            Mean of the distribution.
+
+        :param logscale (optional):
+            Log of std of the distribution.
+
+        :return:
+            Log of pdf for normal distribution with specified parameters. Value
+            or array of values (depends on ``x``).
+        """
+        return -0.5 * math.log(2. * math.pi) - logscale -\
+               (x - loc) ** 2 / (2. * math.exp(logscale) ** 2)
+
+    def __init__(self, beta_ij):
+        self._beta_ij = beta_ij
+
+    def __call__(self, p):
+        """
+        Returns ln of likelihood for kinematic model.
+
+        :param p:
+            Parameters of model. [mu_{G}^{Gamma}, ln(tau_{G}^{Gamma}),
+            mu_{j}^{Gamma}, ln(tau_{j}_{Gamma}), j=1,...,N_{sources}]
+
+        :return:
+            Ln of likelihood function.
+        """
+        k = sum([np.sum(self.ln_of_normpdf(self.y_ij[j], loc=theta,
+                                           logscale=p[2])) for j, theta in
+                 enumerate(p[3:])])
+        return np.sum(self.ln_of_normpdf(p[3:], loc=p[0], logscale=p[1])) + k
+
+
+def lnpr(p):
+    """
+    Prior on parameters.
+
+    :param p:
+        Parameters of model. [mu, log_sigma, log_tau, theta_j j=1,...,N]
+
+    :return:
+        Log of prior density for given parameter ``p``.
+    """
+
+    return p[2]
+
+
 def get_theta_sample(dgamma, a=2, **kwargs):
     """
     Function that generates sample from theta distribution given specified gamma
@@ -58,4 +131,46 @@ def get_theta_sample(dgamma, a=2, **kwargs):
 if __name__ == '__main__':
     dgamma = Dlognorm_shifted(2.)
     np.hist(dgamma(mean=2, sigma=0.5, size=10000), bins=50, normed=True)
-    np.hist(get_theta_sample(dgamma, mean=2, sigma=0.5, size=10000)*180./math.pi, bins=50, normed=True)
+    np.hist(get_theta_sample(dgamma, mean=2, sigma=0.5, size=10000) * 180. /
+            math.pi, bins=50, normed=True)
+
+    # Load data
+    data_lists = list()
+    files = ['source.txt', 'beta.txt', 'sigma_beta.txt']
+    for file_ in files:
+        with open(file_) as f:
+            data_lists.append(f.read().splitlines())
+    sources_list = data_lists[0]
+    betas_list = data_lists[1]
+    beta_sigmas_list = data_lists[2]
+
+    # Convert to float numbers
+    for i, value in enumerate(betas_list):
+        try:
+            betas_list[i] = float(value)
+        except ValueError:
+            betas_list[i] = None
+
+    for i, value in enumerate(beta_sigmas_list):
+        try:
+            beta_sigmas_list[i] = float(value)
+        except ValueError:
+            beta_sigmas_list[i] = None
+
+    #as Bring all data to nested list
+    betas_nested = list()
+    beta_sigmas_nested = list()
+    betas_dict = dict()
+    beta_sigmas_dict = dict()
+    sources_set = set(sources_list)
+    for source in sources_set:
+        # Indices of ``source_silt`` with current source
+        indices = [i for i, x in enumerate(sources_list) if x == source]
+        betas_nested.append([betas_list[i] for i in indices])
+        betas_dict.update({source: [betas_list[i] for i in indices]})
+        beta_sigmas_nested.append([beta_sigmas_list[i] for i in indices])
+        beta_sigmas_dict.update({source: [beta_sigmas_list[i] for i in
+                                          indices]})
+
+
+
